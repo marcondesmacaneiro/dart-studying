8d177742c53c2514c132b7efffa049951fa788a9
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2019-10-19 16:30:05 -0300

    Variáveis Inteiras.

---
+++ 02-variaveis/variaveisInteiras.dart
@@ -0,0 +1,8 @@
+void main() {
+  int idade = 35;
+  int idade_irmao = idade + 2;
+  int idade_avo = (idade + idade_irmao)*3;
+  print("Eu tenho $idade anos.");
+  print("Idade do meu irmão $idade_irmao.");
+  print("Idade do meu avó é $idade_avo.");
+}
